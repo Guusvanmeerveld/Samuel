06059d5b86a9bdfe574edf8d51263042dd2a79ce
Guusvanmeerveld <Guusvanmeerveld@users.noreply.github.com>
2022-01-15 22:59:16 +0000

    deploy: b55a796085bf3f8d3082b7b00eeed2fc208c23d2

---
+++ _next/static/TzWTC_VRj9gMylg14Gkgq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4309734b2556de8c.css","static/chunks/pages/index-27a7b714f0039334.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/aIS1_lwGKsfmWax-QoL9k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8f19d65f65b817d8.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
