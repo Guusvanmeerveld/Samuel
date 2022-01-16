ad5a574914cac3bfc60c51c857355c9cdce0d352
Guusvanmeerveld <Guusvanmeerveld@users.noreply.github.com>
2022-01-16 13:51:14 +0000

    deploy: 2022b133aa5377a1c4f2de29a33b2b739998c10c

--- _next/static/vw26weNFdOW8VwkUcz1sb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4309734b2556de8c.css","static/chunks/pages/index-27a7b714f0039334.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
